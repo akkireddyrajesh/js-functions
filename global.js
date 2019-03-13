eb595506a8431a35e6a40ea3c718c1cd47e898f5
Akkireddy Rajesh <akkireddyrajesh@gmail.com>
2019-03-13 18:51:26 +0530

    //(2) removing duplicate objects from AoO
    removing duplicate objects from AoO

--- global.js
+++ global.js
@@ -23,68 +23,6 @@ let AoB=[{"a":1,"b":2,"c":3},{"a":4,"b":5,"c":6}];
 let eachObj=AoB.find(x => x.a === 1)
 console.log(`eachObj`, eachObj);
 
-//getting particular values array from  AofO
-let elArray= AoB.map(a => a.elArray);
-
-//finding the value in array
-let arr = ['a','b','c'];
-arr.findIndex(k => k=='b');
-
-//finding object in AoO
-//Finding matching objects in an array of objects
-
-//Using Array#filter, for this particular case the code would look like
-// ref: url(https://stackoverflow.com/questions/6237537/finding-matching-objects-in-an-array-of-objects);
-var set = [{"color":"blue"},{"color":"green"},{"color":"red"},{"color":"green"}];
-var results = set.filter(function (entry) { return entry.color === "green"; });
-    // [or]
-function findByMatchingProperties(set, properties) {
-    return set.filter(function (entry) {
-        return Object.keys(properties).every(function (key) {
-            return entry[key] === properties[key];
-        });
-    });
-}
-var results = findByMatchingProperties(set, { color: "green" });
-
-//split string
-//split name into firstName and lastName
-str.substr(0,str.indexOf(' '));
-str.substr(str.indexOf(' ')+1); 
-
-//---------------------- forms ----------------
-//form submit
-$("#formId").submit(function (event) {
-    event.preventDefault();
-    let serialize = $(this).serializeArray();
-    
-//     //handle file data
-//     let uploadedFile = $('#uploadFile').prop('files')[0];
-//     alert(uploadedFile.name);
-    
-    let formObj = {};
-    $.each(serialize, function (key, val) {
-        formObj[val.name] = val.value;
-    });
-    console.log(formObj);
-
-    cbFn(formObj);
-});
-
-//jQuery keypress 
-//Key press functions
-// 1 . default 
-$(document).ready(function(){
-   $('.vaildName').bind('keypress', vaildName);
-});
-//2. if the html elements are created dynamiccaly then    
-$(document).bind("keypress",".validName",vaildNo);
-
-function vaildName(e) {
-    a('key pressed');
-}
-
-
 //Compare two arrays and return a new array with any items only found in one of the original arrays
 //method 1
 var array1 = ["diorite", "andesite", "grass", "dirt", "pink wool", "dead shrub"],
@@ -121,24 +59,70 @@ Array.prototype.diff = function(a) {
 ["test1", "test2","test3","test4","test5","test6"].diff(["test1","test2","test3","test4"]);  
 // => ["test5", "test6"]
 
+//getting particular values array from  AofO
+let elArray= AoB.map(a => a.elArray);
 
-//self invoking function
-(function myLoop (i) {          
-    setTimeout(function () {   
-       alert('hello');          //  your code here                
-       if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
-    }, 3000)
- })(10);                        //  pass the number of iterations as an argument
+//finding the value in array
+let arr = ['a','b','c'];
+arr.findIndex(k => k=='b');
+//----------- AoO ----------
+
+//(1)finding object in AoO
+//Finding matching objects in an array of objects
+//Using Array#filter, for this particular case the code would look like
+// ref: url(https://stackoverflow.com/questions/6237537/finding-matching-objects-in-an-array-of-objects);
+var set = [{"color":"blue"},{"color":"green"},{"color":"red"},{"color":"green"}];
+var results = set.filter(function (entry) { return entry.color === "green"; });
+    // [or]
+function findByMatchingProperties(set, properties) {
+    return set.filter(function (entry) {
+        return Object.keys(properties).every(function (key) {
+            return entry[key] === properties[key];
+        });
+    });
+}
+var results = findByMatchingProperties(set, { color: "green" });
+
+//(2) removing duplicate objects from AoO
+// ref:https://stackoverflow.com/questions/2218999/remove-duplicates-from-an-array-of-objects-in-javascript 
+let things={
+    "thing" :[
+        {place:"here",name:"stuff"},
+        {place:"there",name:"morestuff"},
+        {place:"there",name:"morestuff"}
+    ]
+};
+
+things.thing = things.thing.filter((thing, index, self) =>
+  index === self.findIndex((t) => (
+    t.place === thing.place && t.name === thing.name
+  ))
+)
+// o/p:
+// {place:"here",name:"stuff"},
+// {place:"there",name:"morestuff"}
+
 
 //------------- DATES -----------------------
 // ISO STRING
 var date = new Date();
 date.toISOString(); //"2018-07-03T18:45:44.855Z"
 
+//------------- JQuery -----------------------
 
+//jQuery keypress 
+//Key press functions
+// 1 . default 
+$(document).ready(function(){
+   $('.vaildName').bind('keypress', vaildName);
+});
+//2. if the html elements are created dynamiccaly then    
+$(document).bind("keypress",".validName",vaildNo);
 
+function vaildName(e) {
+    a('key pressed');
+}
 //Download Javascript As Json
-
 $("#data").click(function() {
   $("<a />", {
     "download": "data.json",
@@ -148,11 +132,6 @@ $("#data").click(function() {
      $(this).remove()
   })[0].click()
 });
-
-//first letter capital
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
 
 //Json file upload 
 <input type="file" id="selectFiles" value="Import" />
@@ -167,7 +146,35 @@ function onReaderLoad(event){
     console.log('file uploaded successfully...',obj);
 }
 
+//----------- JQuery forms ------------
+//form submit
+$("#formId").submit(function (event) {
+    event.preventDefault();
+    let serialize = $(this).serializeArray();
+    ////handle file data
+    //let uploadedFile = $('#uploadFile').prop('files')[0];
+    //alert(uploadedFile.name);
+    let formObj = {};
+    $.each(serialize, function (key, val) {
+        formObj[val.name] = val.value;
+    });
+    console.log(formObj);
+    cbFn(formObj);
+});
+
+
 //------------- String handling -----------------------
+
+//split string
+//split name into firstName and lastName
+str.substr(0,str.indexOf(' '));
+str.substr(str.indexOf(' ')+1); 
+
 //string first letter Capitalize
 const name = 'flavio'
 const nameCapitalized = name.charAt(0).toUpperCase() + name.slice(1);
+
+//first letter capital
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
